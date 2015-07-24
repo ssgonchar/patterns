bcdc6bfec979d868a543a8710211c3d3ac2b8d36
Sergey <sg@steeldespair.com>
2015-07-24 07:17:26 +0300

    02. inheritance

--- ShopProduct.php
+++ ShopProduct.php
@@ -8,26 +8,79 @@
  */
 class ShopProduct
 {
-    public $title = 'Стандартный товар';
-    public $producerMainName = 'Фамилия автора';
-    public $producerFirstName = 'Имя автора';
-    public $price = 0;
+    public $numPages;
+    public $playLength;
+    public $title;
+    public $producerMainName;
+    public $producerFirstName;
+    public $price;
 
     function __construct($title,
-                         $firstName, $mainName, $price) {
+                         $firstName, $mainName, $price,
+                         $numPages = 0, $playLength = 0) {
         $this->title = $title;
         $this->producerFirstName = $firstName;
         $this->producerMainName = $mainName;
         $this->price = $price;
+        $this->numPages = $numPages;
+        $this->playLength = $playLength;
     }
 
     function getProducer() {
         return "{$this->producerFirstName} "
         . "{$this->producerMainName}";
     }
+
+    function getSummaryLine() {
+        $base = "$this->title ( {$this->producerMainName}, ";
+        $base .= "{$this->producerFirstName} )";
+
+        return $base;
+    }
+}
+
+class CDProduct extends ShopProduct{
+    function getPlayLength() {
+        return $this->playLength;
+    }
+
+    function getSummaryLine(){
+        $base = "$this->title ( {$this->producerMainName}, ";
+        $base .= "{$this->producerFirstName} )";
+        $base .= ": Время звучания - {$this->playLength} ";
+
+        return $base;
+    }
+}
+
+class BookProduct extends ShopProduct {
+    function getNumberOfPages(){
+        return $this->numPages;
+    }
+
+    function getSummaryLine(){
+        $base = "$this->title ( {$this->producerMainName}, ";
+        $base .= "{$this->producerFirstName} )";
+        $base .= ": {$this->numPages} стр.";
+
+        return $base;
+    }
+}
+
+class ShopProductWriter
+{
+    public function write(ShopProduct $shopProduct) {
+        $str = "{$shopProduct->title}: "
+            . $shopProduct->getProducer()
+            . " ({$shopProduct->price})\n";
+
+        print $str;
+    }
 }
 
 $product1 = new ShopProduct('Собачье сердце',
-                            'Михаил', 'Булгаков', 5.99);
+    'Михаил', 'Булгаков', 5.99);
+$writer = new ShopProductWriter();
+$writer->write($product1);
 
-print "Автор: {$product1->getProducer()}\n";
+//print "Автор: {$product1->getProducer()}\n";
